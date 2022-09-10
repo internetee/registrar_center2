52e30dc910b833385270bd553bb4f806a1590d4e
Sergei Tsõganov <sergeit6@gmail.com>
2022-09-10 10:20:15 +0300

    Improved nested forms for domains

--- app/javascript/controllers/nested_form_controller.js
+++ app/javascript/controllers/nested_form_controller.js
@@ -7,6 +7,8 @@ export default class extends Controller {
                     "nameserver", "dnskeyTarget", "dnskeyTemplate",
                     "dnskey"];
   static values = {
+      maxNumElements: 10,
+      minNumElements: 1,
       contactSelectorWrapper: {
           type: String,
           default: ".contact-nested-form-wrapper"
@@ -23,9 +25,11 @@ export default class extends Controller {
   add(e) {
       e.preventDefault();
       let type = e.target.dataset.value;
-      let num_elements = this.numberOfVisibleElements(this[type + 'SelectorWrapperValue']);
-      if (num_elements < 10) {
-          const content = this[type + "TemplateTarget"].innerHTML.replace(/NEW_RECORD/g, num_elements + 1);
+      let numVisibleElements = this.numberOfElements(this[type + 'SelectorWrapperValue'], true);
+      let numInvisibleElements = this.numberOfElements(this[type + 'SelectorWrapperValue'], false);
+      if (numVisibleElements < this.maxNumElementsValue) {
+          let index = numVisibleElements + numInvisibleElements + 1;
+          const content = this[type + "TemplateTarget"].innerHTML.replace(/NEW_RECORD/g, index);
           this[type + "TargetTarget"].insertAdjacentHTML("beforebegin", content);
           const last = Array.from(
               document.querySelectorAll(this[type + 'SelectorWrapperValue'])
@@ -33,20 +37,24 @@ export default class extends Controller {
           last.querySelectorAll('select:not(.flatpickr-monthDropdown-months)').forEach(elem => {
               new Select(elem, {});
           });
+          this.restartCount(type);
       }
   }
   remove(e) {
       e.preventDefault();
       let type = e.target.dataset.value;
       const wrapper = e.target.closest(this[type + 'SelectorWrapperValue']);
-      let num_elements = this.numberOfVisibleElements(this[type + 'SelectorWrapperValue']);
-      if (num_elements > 0) {
+      let numVisibleElements = this.numberOfElements(this[type + 'SelectorWrapperValue'], true);
+      if (numVisibleElements >= this.minNumElementsValue) {
           if (wrapper.dataset.newRecord === 'true') {
               wrapper.remove();
           } else {
               wrapper.style.display = 'none';
               const input = wrapper.querySelector("input[name*='action']");
               input.value = 'rem';
+          }
+          if (numVisibleElements < 2) {
+              this.add(e);
           }
           this.restartCount(type);
       }
@@ -56,21 +64,23 @@ export default class extends Controller {
       const label = e.target.closest(this.contactSelectorWrapperValue).querySelector(".form--label");
       label.innerHTML = e.target.dataset.value;
   }
-  numberOfVisibleElements(elem) {
-      let count = 0;
-      document.querySelectorAll(elem).forEach(elem => {
-          if (elem.style.display != 'none') {
-              count += 1;
-          }
+  numberOfElements(elem, visible) {
+      var divs = document.querySelectorAll(elem);
+      var divsArray = [].slice.call(divs);
+      var filtered = divsArray.filter(function(el) {
+          let display = getComputedStyle(el).display;
+          return visible ? display !== 'none' : display === 'none';
       });
-      return count;
+      return filtered.length;
   }
   restartCount(type) {
     let count = 1;
     let elements = document.querySelectorAll(this[type + 'SelectorWrapperValue']);
     elements.forEach((elem) => {
-        if (elem.style.display != 'none') {
-            let header = elem.querySelector("h3").innerHTML.replace(/[0-9]/g, count > 1 ? count : '');
+        if (elem.style.display !== 'none') {
+            let index = count > 1 ? count : '';
+            let html = elem.querySelector("h3").innerHTML;
+            let header = /\d+/.test(html) ? html.replace(/\d+/g, index) : html + ' ' + index;
             elem.querySelector("h3").innerHTML = header;
             count += 1;
         }
